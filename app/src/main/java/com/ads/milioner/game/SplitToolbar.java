3c6d99e4db59c35be1a145376c963610714961d2
sigma <taleb.zarhesh@gmail.com>
2019-01-20 02:09:43 +0330

    add game to app

---
+++ app/src/main/java/com/ads/milioner/game/SplitToolbar.java
@@ -0,0 +1,34 @@
+package com.ads.milioner.game;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.appcompat.widget.ActionMenuView;
+import androidx.appcompat.widget.Toolbar;
+
+/**
+ * Created by AarKay
+ */
+public class SplitToolbar extends Toolbar {
+
+    public SplitToolbar(Context context) {
+        super(context);
+    }
+
+    public SplitToolbar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SplitToolbar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public void addView(View child, ViewGroup.LayoutParams params) {
+        if (child instanceof ActionMenuView) {
+            params.width = LayoutParams.MATCH_PARENT;
+        }
+        super.addView(child, params);
+    }
+}
